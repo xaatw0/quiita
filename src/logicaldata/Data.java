9f96011e1f6a25aedbf9b9a4baa825b9859b2550
xaatw0 <>
2015-07-07 10:28:19 +0900

    test2

--- src/logicaldata/Data.java
+++ src/logicaldata/Data.java
@@ -2,4 +2,19 @@ package logicaldata;
 
 public class Data {
 
+	private int x;
+	private int y;
+
+	public Data(int x, int y){
+		this.x = x;
+		this.y = y;
+	}
+
+	public int getX(){
+		return x;
+	}
+
+	public int getY(){
+		return y;
+	}
 }

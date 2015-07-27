c438b781017e025583c812e5278f72a50d623640
xaatw <>
2015-07-27 11:49:35 +0900

    JavaFXの最小の構成

---
+++ src/lightestfx/FXController.java
@@ -0,0 +1,13 @@
+package lightestfx;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+
+public class FXController implements Initializable{
+
+	@Override
+	public void initialize(URL paramURL, ResourceBundle paramResourceBundle) {
+	}
+}

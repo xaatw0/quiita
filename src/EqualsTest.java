492f6766460fd048a4200a1e311d953a740712e4
s-sakurama <s-sakurama@PC-K115>
2015-10-09 10:11:01 +0900

    boolean追加

--- src/EqualsTest.java
+++ src/EqualsTest.java
@@ -7,17 +7,19 @@ public class EqualsTest {
 
 	private String name;
 	private int age;
-
-	public EqualsTest(){}
+	private boolean isMan;
 
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
 		result = prime * result + age;
+		result = prime * result + (isMan ? 1231 : 1237);
 		result = prime * result + ((name == null) ? 0 : name.hashCode());
 		return result;
 	}
+
+
 
 	@Override
 	public boolean equals(Object obj) {
@@ -30,6 +32,8 @@ public class EqualsTest {
 		EqualsTest other = (EqualsTest) obj;
 		if (age != other.age)
 			return false;
+		if (isMan != other.isMan)
+			return false;
 		if (name == null) {
 			if (other.name != null)
 				return false;
@@ -37,6 +41,7 @@ public class EqualsTest {
 			return false;
 		return true;
 	}
+
 
 	public boolean myEquals(Object obj){
 
@@ -46,6 +51,7 @@ public class EqualsTest {
 		EqualsTest other = (EqualsTest) obj;
 
 		boolean result = true;
+		result = result && (isMan == other.isMan);
 		result = result && (age == other.age);
 		result = result && (name == null ? other.name == null : name.equals(other.name));
 
@@ -56,34 +62,63 @@ public class EqualsTest {
 	@Test
 	public void equalsTest(){
 
+		// null, 異なるオブジェクト、自分自身との比較
 		EqualsTest target1 = new EqualsTest();
 		assertThat(target1.equals(null), is(false));
+		assertThat(target1.equals("test"), is(false));
+		assertThat(target1.equals(target1), is(true));
 
+		// 内容が同じものと比較
+		// [name:target1=null target2=null  age: target1=0 target2=0]
 		EqualsTest target2 = new EqualsTest();
 		assertThat(target1.equals(target2), is(true));
 
+		// nameが異なる
+		// [name:target1="name" target2=null  age: target1=0 target2=0]
 		target1.name = "name";
 		assertThat(target1.equals(target2), is(false));
 
+		// 内容が同じものと比較
+		// [name:target1="name" target2="name"  age: target1=0 target2=0]
 		target2.name = "name";
 		assertThat(target1.equals(target2), is(true));
 
+		// nameが異なる
+		// [name:target1="name2" target2="name"  age: target1=0 target2=0]
 		target1.name = "name2";
 		assertThat(target1.equals(target2), is(false));
 
+		// 内容が同じものと比較
+		// [name:target1="name2" target2="name2"  age: target1=0 target2=0]
 		target2.name = "name2";
 		assertThat(target1.equals(target2), is(true));
 
+		// ageが異なる
+		// [name:target1="name2" target2="name2"  age: target1=15 target2=0]
 		target1.age = 15;
 		assertThat(target1.equals(target2), is(false));
 
+		// 内容が同じものと比較
+		// [name:target1="name2" target2="name2"  age: target1=15 target2=15]
 		target2.age = 15;
 		assertThat(target1.equals(target2), is(true));
 
+		// nameが異なる
+		// [name:target1=null target2="name2"  age: target1=15 target2=15]
 		target1.name = null;
 		assertThat(target1.equals(target2), is(false));
 
+		// 内容が同じものと比較
+		// [name:target1=null target2=null  age: target1=15 target2=15]
 		target2.name = null;
+		assertThat(target1.equals(target2), is(true));
+
+		// isManが異なる
+		target1.isMan = true;
+		assertThat(target1.equals(target2), is(false));
+
+		// 内容が同じものと比較
+		target2.isMan = true;
 		assertThat(target1.equals(target2), is(true));
 	}
 
@@ -91,34 +126,63 @@ public class EqualsTest {
 	@Test
 	public void myEqualsTest(){
 
+		// null, 異なるオブジェクト、自分自身との比較
 		EqualsTest target1 = new EqualsTest();
 		assertThat(target1.myEquals(null), is(false));
+		assertThat(target1.myEquals("test"), is(false));
+		assertThat(target1.myEquals(target1), is(true));
 
+		// 内容が同じものと比較
+		// [name:target1=null target2=null  age: target1=0 target2=0]
 		EqualsTest target2 = new EqualsTest();
 		assertThat(target1.myEquals(target2), is(true));
 
+		// nameが異なる
+		// [name:target1="name" target2=null  age: target1=0 target2=0]
 		target1.name = "name";
 		assertThat(target1.myEquals(target2), is(false));
 
+		// 内容が同じものと比較
+		// [name:target1="name" target2="name"  age: target1=0 target2=0]
 		target2.name = "name";
 		assertThat(target1.myEquals(target2), is(true));
 
+		// nameが異なる
+		// [name:target1="name2" target2="name"  age: target1=0 target2=0]
 		target1.name = "name2";
 		assertThat(target1.myEquals(target2), is(false));
 
+		// 内容が同じものと比較
+		// [name:target1="name2" target2="name2"  age: target1=0 target2=0]
 		target2.name = "name2";
 		assertThat(target1.myEquals(target2), is(true));
 
+		// ageが異なる
+		// [name:target1="name2" target2="name2"  age: target1=15 target2=0]
 		target1.age = 15;
 		assertThat(target1.myEquals(target2), is(false));
 
+		// 内容が同じものと比較
+		// [name:target1="name2" target2="name2"  age: target1=15 target2=15]
 		target2.age = 15;
 		assertThat(target1.myEquals(target2), is(true));
 
+		// nameが異なる
+		// [name:target1=null target2="name2"  age: target1=15 target2=15]
 		target1.name = null;
 		assertThat(target1.myEquals(target2), is(false));
 
+		// 内容が同じものと比較
+		// [name:target1=null target2=null  age: target1=15 target2=15]
 		target2.name = null;
+		assertThat(target1.myEquals(target2), is(true));
+
+		// isManが異なる
+		target1.isMan = true;
+		assertThat(target1.myEquals(target2), is(false));
+
+		// 内容が同じものと比較
+		target2.isMan = true;
 		assertThat(target1.myEquals(target2), is(true));
 	}
 

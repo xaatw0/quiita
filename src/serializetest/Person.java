e857e05a8de0f4f6b6d0155dbf47cf6be06ad9c6
s-sakurama <s-sakurama@PC-K115>
2015-10-28 14:17:55 +0900

    シリアライズのテストを追加

---
+++ src/serializetest/Person.java
@@ -0,0 +1,34 @@
+package serializetest;
+
+import java.io.Serializable;
+
+
+public class Person implements Serializable{
+
+	private String name;
+	private int age;
+
+	public Person(String name, int age){
+		this.name = name;
+		this.age = age;
+	}
+
+	public String getName(){return name;}
+	public int getAge(){ return age;}
+
+
+	private void writeObject(java.io.ObjectOutputStream stream)
+	        throws java.io.IOException {
+	    age = age >> 2;
+	    stream.defaultWriteObject();
+	}
+
+    private void readObject(java.io.ObjectInputStream stream)
+        throws java.io.IOException, ClassNotFoundException {
+
+    	stream.defaultReadObject();
+        age = age << 2;
+    }
+
+}
+

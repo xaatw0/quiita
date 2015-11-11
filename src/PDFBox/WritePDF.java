6e9b31fddd0ff4b32d73f8260a579ec0ed466c86
xaatw0 <xaatw0>
2015-11-11 15:53:46 +0900

    出力を追加

--- src/PDFBox/WritePDF.java
+++ src/PDFBox/WritePDF.java
@@ -9,30 +9,121 @@ import org.apache.pdfbox.pdfparser.PDFParser;
 import org.apache.pdfbox.pdfwriter.COSWriter;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.PDPageContentStream;
+import org.apache.pdfbox.pdmodel.font.PDFont;
+import org.apache.pdfbox.pdmodel.font.PDType0Font;
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class WritePDF {
 
 	@Test
-	public void writeread() throws IOException{
+	public void writeread() throws IOException {
 
-		ByteArrayOutputStream out = new ByteArrayOutputStream();
+		byte[] buffer;
 
-	    //PDFドキュメントを作成
-	    PDDocument document = new PDDocument();
-	    //ページを追加
-	    document.addPage(new PDPage());
+		// PDFの書き込み
+		try (ByteArrayOutputStream out = new ByteArrayOutputStream()) {
 
-	    try(COSWriter writer = new COSWriter(out)){
-	    	writer.write(document);
-	    }
+			// PDFドキュメントを作成
+			PDDocument document = new PDDocument();
 
-	    try(RandomAccessRead read = new RandomAccessBuffer(out.toByteArray())){
+			// ページを追加
+			document.addPage(new PDPage());
 
-		     PDFParser pdfParser = new PDFParser(read);
-		     pdfParser.parse(); // 分析
-		     PDDocument pdf = pdfParser.getPDDocument();
-	    }
+			try (COSWriter writer = new COSWriter(out)) {
+				writer.write(document);
+			}
+
+			buffer = out.toByteArray();
+		}
+
+		// PDFの読み込み
+		try (RandomAccessRead read = new RandomAccessBuffer(buffer)) {
+
+			PDFParser pdfParser = new PDFParser(read);
+			pdfParser.parse(); // 分析
+			PDDocument pdf = pdfParser.getPDDocument();
+		}
+	}
+
+	@Test @Ignore
+	public void write1行() throws IOException {
+
+		try (PDDocument doc = new PDDocument()) {
+
+			// 新しいページを追加
+			PDPage page = new PDPage();
+			doc.addPage(page);
+
+			// フォントファイルを読み込み
+			PDFont font = PDType0Font.load(doc, getClass().getResourceAsStream("TanukiMagic.ttf"));
+
+			// 出力用のストリームを開く
+			try (PDPageContentStream contents = new PDPageContentStream(doc, page)) {
+
+				// テキスト出力開始
+				contents.beginText();
+
+				// 使用するフォントに読み込んだフォントを指定。
+				// 第二引数はフォントサイズ
+				contents.setFont(font, 46);
+
+				// テキストの出力位置を指定。
+				// 左下がX:0 Y:0の座標となる
+				contents.newLineAtOffset(100, 500);
+				contents.showText("日本語出力123");
+				// テキスト出力終了
+				contents.endText();
+
+			}
+			// pdfファイルを出力
+			doc.save("sample.pdf");
+		}
+	}
+
+	@Test @Ignore
+	public void write2行() throws IOException {
+
+		try (PDDocument doc = new PDDocument()) {
+
+			// 新しいページを追加
+			PDPage page = new PDPage();
+			doc.addPage(page);
+
+			// フォントファイルを読み込み
+			PDFont font = PDType0Font.load(doc, getClass().getResourceAsStream("TanukiMagic.ttf"));
+
+			// 出力用のストリームを開く
+			try (PDPageContentStream contents = new PDPageContentStream(doc, page)) {
+
+			      // 行間隔を指定
+		        contents.setLeading(46 * 1.2);
+
+		        // テキスト出力開始
+		        contents.beginText();
+
+		        // 使用するフォントに読み込んだフォントを指定。
+		        // 第二引数はフォントサイズ
+		        contents.setFont(font, 46);
+
+		        // テキストの出力位置を指定。
+		        // 左下がX:0 Y:0の座標となる
+		        contents.newLineAtOffset(100, 500);
+		        contents.showText("日本語出力123");
+
+		        // 改行
+		        contents.newLine();
+		        contents.showText("二行目を出力！");
+
+				// テキスト出力終了
+				contents.endText();
+
+			}
+
+			// pdfファイルを出力
+			doc.save("sample2.pdf");
+		}
 	}
 
 }

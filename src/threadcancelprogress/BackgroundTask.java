8f9002db36f6c1c340cde6d085b06a0581461337
xaatw0 <xaatw0@gmail.com>
2016-03-14 15:02:10 +0900

    コメントの追加とラムダ式化

--- src/threadcancelprogress/BackgroundTask.java
+++ src/threadcancelprogress/BackgroundTask.java
@@ -13,6 +13,9 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 
 
+/**
+ * 実施、キャンセル、進捗状況報告を実装するタスク
+ */
 public abstract class BackgroundTask<V> implements Runnable, Future<V> {
 
 	private final FutureTask<V> computation = new Computation();
@@ -29,61 +32,69 @@ public abstract class BackgroundTask<V> implements Runnable, Future<V> {
 			});
 		}
 
+		@Override
 		protected final void done(){
-			Platform.runLater(new Runnable() {
+			Platform.runLater(() -> {
 
-				@Override
-				public void run() {
-					V value = null;
-					Throwable thrown = null;
-					boolean canceled = false;
+				V value = null;
+				Throwable thrown = null;
+				boolean canceled = false;
 
-					try{
-						value = get();
+				try {
+					value = get();
+				} catch(ExecutionException ex){
+					thrown = ex.getCause();
+				} catch(CancellationException ex){
+					canceled = true;
+				} catch(InterruptedException ex){
 
-					}catch(ExecutionException ex){
-						thrown = ex.getCause();
-					}catch(CancellationException ex){
-						canceled = true;
-					}catch(InterruptedException ex){
-
-					}finally{
-						onCompletion(value, thrown, canceled);
-					}
+				} finally{
+					onCompletion(value, thrown, canceled);
 				}
 			});
 		}
-
 	}
 
 	@Override
-	public void run() {
+	public final void run() {
 		computation.run();
 	}
 
-	protected void start(){
-		Platform.runLater(()-> onStart());;
+	/**
+	 * 開始時の処理をアプリケーションスレッドに登録する
+	 */
+	private void start(){
+		Platform.runLater(() -> onStart());;
 	}
 
 	/**
-	 * computeメソッドから呼び出し、進捗状況を数量的に表示する
-	 * @param current
-	 * @param max
+	 * computeメソッド(バックグラウンドスレッド)から呼び出し、進捗状況を数量的に表示する
+	 * @param current 現状の値
+	 * @param max 完了の値
 	 */
 	protected void setProgress(final int current, final int max){
-
 		Platform.runLater(() -> onProgress(current, max));
 	}
 
-
-
-	// バックグラウンドスレッドで呼ばれる
+	/**
+	 * 実際の処理を実装する。バックグラウンドスレッドで呼ばれる。<br/>
+	 * メソッド内でsetProgressを使用して、画面上の進捗状況を更新する。
+	 * @return 計算結果
+	 */
 	protected abstract V compute();
 
-	// イベントスレッドスレッドで呼ばれる
+	/**
+	 * 処理完了時に実施され、画面上に結果を反映させる。<br/>
+	 * JavaFXのアプリケーションスレッドから呼ばれる
+	 */
 	protected void onCompletion(V result, Throwable exception, boolean canceled){}
 
-
+	/**
+	 * 処理開始時に実施され、画面を反映させる。<br/>
+	 * JavaFXのアプリケーションスレッドから呼ばれる。
+	 * @param current
+	 * @param max
+	 */
 	protected void onStart(){}
 
 	/**
@@ -95,39 +106,38 @@ public abstract class BackgroundTask<V> implements Runnable, Future<V> {
 
 
 	@Override
-	public boolean cancel(boolean mayInterruptIfRunning) {
+	public final boolean cancel(boolean mayInterruptIfRunning) {
 		return computation.cancel(mayInterruptIfRunning);
 	}
 
 	@Override
-	public boolean isCancelled() {
+	public final boolean isCancelled() {
 		return computation.isCancelled();
 	}
 
 	@Override
-	public boolean isDone() {
+	public final boolean isDone() {
 		return computation.isDone();
 	}
 
 	@Override
-	public V get() throws InterruptedException, ExecutionException {
+	public final V get() throws InterruptedException, ExecutionException {
 		return computation.get();
 	}
 
 	@Override
-	public V get(long timeout, TimeUnit unit)
+	public final V get(long timeout, TimeUnit unit)
 			throws InterruptedException, ExecutionException,
 			TimeoutException {
 		return computation.get(timeout, unit);
 	}
 
-	public EventHandler<ActionEvent> getCancelEvent(){
-
-		 return new EventHandler<ActionEvent>() {
-				@Override
-				public void handle(ActionEvent event) {
-					BackgroundTask.this.cancel(true);
-				}
-		 };
+	/**
+	 * キャンセルイベントのハンドラを取得する<br/>
+	 * キャンセルボタンのsetOnActionに設定する。
+	 * @return キャンセルイベントのハンドラ
+	 */
+	protected final EventHandler<ActionEvent> getCancelEvent(){
+		return event -> BackgroundTask.this.cancel(true);
 	}
 }

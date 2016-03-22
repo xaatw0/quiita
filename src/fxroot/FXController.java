2390066f5ab26a66435235d07058b2e06b98b125
xaatw0 <xaatw0@gmail.com>
2016-03-22 13:17:48 +0900

    部分パーツ化を実装

---
+++ src/fxroot/FXController.java
@@ -0,0 +1,13 @@
+package fxroot;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+
+public class FXController implements Initializable{
+
+	@Override
+	public void initialize(URL paramURL, ResourceBundle paramResourceBundle) {
+	}
+}

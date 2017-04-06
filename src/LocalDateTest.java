c6613c8566f3ae107396cadf7c4bb92758704d02
xaatw0 <xaatw0@gmail.com>
2017-04-06 13:50:45 +0900

    随時追加していた

--- src/LocalDateTest.java
+++ src/LocalDateTest.java
@@ -6,7 +6,11 @@ import java.time.LocalDate;
 import java.time.Month;
 import java.time.Period;
 import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.TemporalAccessor;
+import java.time.temporal.WeekFields;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 /**
@@ -16,7 +20,7 @@ import org.junit.Test;
  */
 public class LocalDateTest {
 
-	@Test
+	@Test @Ignore("nowは動的に変化するため、実施しない")
 	public void now(){
 		// 2015/07/27に実施
 		LocalDate date = LocalDate.now();
@@ -133,6 +137,10 @@ public class LocalDateTest {
 		assertThat(period.getYears(),is(-1));
 		assertThat(period.getMonths(),is(-1));
 		assertThat(period.getDays(),is(-1));
+
+		assertThat(ChronoUnit.YEARS.between(start, end), is(1L));
+		assertThat(ChronoUnit.MONTHS.between(start, end), is(12L));
+		assertThat(ChronoUnit.DAYS.between(start, end), is(366L));
 	}
 
 	@Test
@@ -165,6 +173,12 @@ public class LocalDateTest {
 
 		LocalDate date = LocalDate.of(2015, 8, 18);
 		assertThat(date.format(formatter), is("2015/08/18"));
+
+		TemporalAccessor parsed = formatter.parse("2016/12/07");
+		assertThat(LocalDate.from(parsed), is(LocalDate.of(2016, 12, 7)));
+		assertThat(parsed.query(LocalDate::from), is(LocalDate.of(2016, 12, 7)));
+
+		assertThat(formatter.parse("2016/12/07", LocalDate::from), is(LocalDate.of(2016, 12, 7)));
 	}
 
 	@Test
@@ -177,5 +191,87 @@ public class LocalDateTest {
 		assertThat(firstDayOfWeek.getYear(), is(2015));
 		assertThat(firstDayOfWeek.getMonthValue(), is(9));
 		assertThat(firstDayOfWeek.getDayOfMonth(), is(13));
+	}
+
+	@Test
+	public void 何週目か(){
+
+		// ISO基準で何週目か
+		WeekFields wf = WeekFields.ISO;
+	    LocalDate ld = LocalDate.now()
+	            .with(wf.weekBasedYear(), 2016)
+	            .with(wf.weekOfWeekBasedYear(), 10) //第10週の
+	            .with(wf.dayOfWeek(), DayOfWeek.WEDNESDAY.getValue()); //水曜日
+
+	    assertThat(ld, is(LocalDate.of(2016,3,9)));
+
+	    ld = LocalDate.now()
+	            .with(wf.weekBasedYear(), 2016)
+	            .with(wf.weekOfWeekBasedYear(), 37);
+	    assertThat(ld, is(LocalDate.of(2016,9,14)));
+
+	    ld = LocalDate.now()
+	            .with(wf.weekBasedYear(), 2016)
+	            .with(wf.weekOfWeekBasedYear(), 37)
+        		.with(wf.dayOfWeek(), DayOfWeek.WEDNESDAY.getValue()); //水曜日
+	    assertThat(ld, is(LocalDate.of(2016,9,14)));
+
+
+	    // 2016年37週 月:12 火:13 水:14 木:15 金:16 土:17 日:18
+	    LocalDate day = LocalDate.now()
+	            .with(wf.weekBasedYear(), 2016)
+	            .with(wf.weekOfWeekBasedYear(), 37);
+	    assertThat(day.with(wf.dayOfWeek(), DayOfWeek.MONDAY.getValue()), is(LocalDate.of(2016,9,12)));
+	    assertThat(day.with(wf.dayOfWeek(), DayOfWeek.SATURDAY.getValue()), is(LocalDate.of(2016,9,17)));
+	    assertThat(day.with(wf.dayOfWeek(), DayOfWeek.SUNDAY.getValue()), is(LocalDate.of(2016,9,18)));
+
+	    // 2016年1週 月:4 火:5 水:6 木:7 金:8 土:9 日:10
+	    day = LocalDate.now()
+	            .with(wf.weekBasedYear(), 2016)
+	            .with(wf.weekOfWeekBasedYear(), 1);
+	    assertThat(day.with(wf.dayOfWeek(), DayOfWeek.MONDAY.getValue()), is(LocalDate.of(2016,1,4)));
+	    assertThat(day.with(wf.dayOfWeek(), DayOfWeek.SATURDAY.getValue()), is(LocalDate.of(2016,1,9)));
+	    assertThat(day.with(wf.dayOfWeek(), DayOfWeek.SUNDAY.getValue()), is(LocalDate.of(2016,1,10)));
+
+	    /** 0週目、実行時エラー[1-53,54]のみ
+	    day = LocalDate.now()
+	            .with(wf.weekBasedYear(), 2016)
+	            .with(wf.weekOfWeekBasedYear(), 0);**/
+
+	    // 2015年53週 月:28 火:29 水:30 木:31 金:1 土:2 日:3
+	    day = LocalDate.now()
+	            .with(wf.weekBasedYear(), 2015)
+	            .with(wf.weekOfWeekBasedYear(), 53);
+	    assertThat(day.with(wf.dayOfWeek(), DayOfWeek.MONDAY.getValue()), is(LocalDate.of(2015,12,28)));
+	    assertThat(day.with(wf.dayOfWeek(), DayOfWeek.SATURDAY.getValue()), is(LocalDate.of(2016,1,2)));
+	    assertThat(day.with(wf.dayOfWeek(), DayOfWeek.SUNDAY.getValue()), is(LocalDate.of(2016,1,3)));
+
+	    // 日曜日を含む週が1週目で何週目か
+	    wf = WeekFields.SUNDAY_START;
+	    day = LocalDate.now()
+	            .with(wf.weekBasedYear(), 2016)
+	            .with(wf.weekOfWeekBasedYear(), 1);
+	    assertThat(day.with(wf.dayOfWeek(), DayOfWeek.MONDAY.getValue()), is(LocalDate.of(2015,12,27)));
+	    assertThat(day.with(wf.dayOfWeek(), DayOfWeek.SATURDAY.getValue()), is(LocalDate.of(2016,1,1)));
+	    assertThat(day.with(wf.dayOfWeek(), DayOfWeek.SUNDAY.getValue()), is(LocalDate.of(2016,1,2)));
+	}
+
+	@Test
+	public void testfor(){
+		int count = 0;
+		LocalDate from = LocalDate.of(2014, 1, 1);
+		LocalDate to = LocalDate.of(2015, 1, 1);
+		for (LocalDate index = from; from.isBefore(to); index = index.plusMonths(1)){
+			count ++;
+		}
+
+		assertThat(count, is(12));
+	}
+
+	@Test
+	public void minusMonth(){
+		assertThat(LocalDate.of(2017, 2, 28).minusMonths(1), is(LocalDate.of(2017, 1, 28)));
+		assertThat(LocalDate.of(2017, 3, 29).minusMonths(1), is(LocalDate.of(2017, 2, 28)));
+		assertThat(LocalDate.of(2017, 3, 30).minusMonths(1), is(LocalDate.of(2017, 2, 28)));
 	}
 }

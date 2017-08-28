a5bc53d9bc84a2ae61ac079b27b1192ee9382af3
xaatw0 <xaatw0@gmail.com>
2017-08-28 10:32:52 +0900

    チェックボックスでアイコンが変更されるアプリのサンプルを作った。

---
+++ src/fxicon/FXController.java
@@ -0,0 +1,41 @@
+package fxicon;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.beans.binding.When;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.CheckBox;
+import javafx.scene.image.Image;
+
+public class FXController implements Initializable{
+
+	private final static String PATH_ICON_CHECKED = "star.png";
+
+	private final static String PATH_ICON_NO_CHECK = "x.png";
+
+	@FXML
+	private CheckBox check;
+
+	@Override
+	public void initialize(URL paramURL, ResourceBundle paramResourceBundle) {
+	}
+
+	public void setIconProperty(ObservableList<Image> icons){
+
+		ObjectProperty<Image> icon = new SimpleObjectProperty<>();
+		icon.bind(new When(check.selectedProperty())
+				.then(new Image(getClass().getResource(PATH_ICON_CHECKED).toString()))
+				.otherwise(new Image(getClass().getResource(PATH_ICON_NO_CHECK).toString())));
+		icons.add(icon.get());
+
+		icon.addListener( (o, oldValue, newValue) -> {
+			icons.remove(oldValue);
+			icons.add(newValue);
+		});
+	}
+}
---
+++ src/fxicon/FXMain.java
@@ -0,0 +1,36 @@
+package fxicon;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class FXMain extends Application {
+
+	private final String FXML_NAME = "FXMain.fxml";
+
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+
+			FXMLLoader loader = new FXMLLoader();
+			loader.load(getClass().getResource(FXML_NAME).openStream());
+
+			FXController controller = loader.getController();
+			controller.setIconProperty(primaryStage.getIcons());
+
+			BorderPane root = loader.getRoot();
+			Scene scene = new Scene(root);
+			primaryStage.setScene(scene);
+			primaryStage.show();
+
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
